39dd57d3f1d1912dcd5eacc2c140caae21226ff2
Никита шарапов <nikitasharpov147@gmail.com>
2020-10-01 18:06:57 +0500

    build4

---
+++ precache-manifest.b67c293466a727a7e13580a6fa352a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6b4b81ef1ff3fee102953c3e903548a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4691b5c1ed8e83641aa",
+    "url": "/static/css/main.5605fd26.chunk.css"
+  },
+  {
+    "revision": "982cd92881c8d0dd2e3b",
+    "url": "/static/js/2.72e7d932.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72e7d932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4691b5c1ed8e83641aa",
+    "url": "/static/js/main.13991159.chunk.js"
+  },
+  {
+    "revision": "9b12f03f412b5377ea8e",
+    "url": "/static/js/runtime-main.f37dd4a2.js"
+  }
+]);
